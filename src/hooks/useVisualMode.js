7b4b1d28234ee61c5a22de58e740664ec5df961d
Ted <ted@ted-ubuntu-desktop>
2021-04-15 15:57:39 -0600

    add limit to back function

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 export default function useVisualMode(initial) {
   
   const [history, setHistory] = useState([initial]);
+ 
   // creates a copy of history to not mutate history
   const newHistory = [...history];
 
@@ -15,7 +16,9 @@ export default function useVisualMode(initial) {
 
   // pops the last mode in the history
   const back = function() {
-    newHistory.pop()
+    if (newHistory.length > 1) {
+      newHistory.pop();
+    }
 
     setHistory(newHistory);
   }
