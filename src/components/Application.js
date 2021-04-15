7fe54f717a768b2218ba576cd16b4a827950abe7
Ted <ted@ted-ubuntu-desktop>
2021-04-14 19:14:38 -0600

    add getAppointmentsForDay selectors

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,83 +1,84 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 import "components/Application.scss";
 import DayList from "components/DayList";
 import Appointment from "components/Appointment";
 
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
+// stores the api URL for less mess.
+const daysURL = 'http://localhost:8001/api/days';
 
-const appointments = [
-  {
-    id: 1,
-    time: "12pm",
-  },
-  {
-    id: 2,
-    time: "1pm",
-    interview: {
-      student: "Lydia Miller-Jones",
-      interviewer: {
-        id: 1,
-        name: "Sylvia Palmer",
-        avatar: "https://i.imgur.com/LpaY82x.png",
-      }
-    }
-  },
-  {
-    id: 3,
-    time: "2pm",
-    interview: {
-      student: "Archie Cohen",
-      interviewer: {
-        id: 2,
-        name: "Tori Malcolm",
-        avatar: "https://i.imgur.com/Nmx0Qxo.png",
-      }
-    }
-  },
-  {
-    id: 4,
-    time: "3pm",
-    interview: {
-      student: "Maria Boucher",
-      interviewer: {
-        id: 3,
-        name: "Mildred Nazir",
-        avatar: "https://i.imgur.com/T2WwVfS.png",
-      }
-    }
-  },
-  {
-    id: 5,
-    time: "4pm"
-  }
-];
+// const appointments = [
+//   {
+//     id: 1,
+//     time: "12pm",
+//   },
+//   {
+//     id: 2,
+//     time: "1pm",
+//     interview: {
+//       student: "Lydia Miller-Jones",
+//       interviewer: {
+//         id: 1,
+//         name: "Sylvia Palmer",
+//         avatar: "https://i.imgur.com/LpaY82x.png",
+//       }
+//     }
+//   },
+//   {
+//     id: 3,
+//     time: "2pm",
+//     interview: {
+//       student: "Archie Cohen",
+//       interviewer: {
+//         id: 2,
+//         name: "Tori Malcolm",
+//         avatar: "https://i.imgur.com/Nmx0Qxo.png",
+//       }
+//     }
+//   },
+//   {
+//     id: 4,
+//     time: "3pm",
+//     interview: {
+//       student: "Maria Boucher",
+//       interviewer: {
+//         id: 3,
+//         name: "Mildred Nazir",
+//         avatar: "https://i.imgur.com/T2WwVfS.png",
+//       }
+//     }
+//   },
+//   {
+//     id: 5,
+//     time: "4pm"
+//   }
+// ];
 
 export default function Application(props) {
-  const [day, setDay] = useState("Monday");
-  const appointmentsList = appointments.map(appointment => {
+  
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+    appointments: {}
+  });
+
+  const dailyAppointments = [];
+
+  //  used alias to separate the function
+  const setDay = day => setState(prevState => ({...prevState, day }));
+
+  // uses a axios to make a get request to the api url
+  useEffect(() => {
+    axios.get(daysURL)
+      .then((response) => {
+        setState(prevState => ({...prevState, days: response.data}))
+      })
+  }, [])
+
+  // uses array.map to map through the appointments list
+  dailyAppointmentsList.map(appointment => {
     return (
-      // <Appointment
-      // key={appointment.id}
-      // time={appointment.time}
-      // interview={appointment.interview || null}
-      // />
       <Appointment key={appointment.id} {...appointment} />
     );
   });
@@ -94,8 +95,8 @@ export default function Application(props) {
       <hr className="sidebar__separator sidebar--centered" />
       <nav className="sidebar__menu">
       <DayList
-        days={days}
-        day={day}
+        days={state.days}
+        day={state.day}
         setDay={setDay}
       />
       </nav>
---
+++ src/helpers/selectors.js
@@ -0,0 +1,31 @@
+
+export function getAppointmentsForDay(state, day) {
+  
+  let result = [];
+  let appointments;
+  const daysArr = state.days
+  const appsArr = state.appointments
+  
+  // loops through daysArr to find a matching day
+  for (let arr of daysArr){
+    if (arr.name === day){
+      appointments = arr.appointments
+    }
+  }
+  
+  // checks to see if appointments is undefined
+  if (!appointments) {
+    appointments = [];
+  } else {
+    for (let arr in appsArr){
+      for (let app of appointments){
+        if (arr == app) {
+          result.push(appsArr[arr])
+        }
+      }
+    }
+  }
+  
+  return result;
+  
+}
