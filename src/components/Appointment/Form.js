b4cf982234f61c3c2da3ccbcd62cbeeeacc3218c
Ted <ted@ted-ubuntu-desktop>
2021-04-15 16:59:26 -0600

    add back in onCancel props in <Form />

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
-import InterviewerList from "components/InterviewerList";
-import Button from "components/Button";
+import InterviewerList     from "components/InterviewerList";
+import Button              from "components/Button";
 
 export default function Form(props) {
   
--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,32 +1,33 @@
-import React, { useEffect } from "react";
+import React                from "react";
 import Header               from "components/Appointment/Header";
 import Empty                from "components/Appointment/Empty";
 import Show                 from "components/Appointment/Show";
+import Form                 from "components/Appointment/Form";
 import useVisualMode        from "../../hooks/useVisualMode";
 
 import "components/Appointment/styles.scss";
 
-const EMPTY = "EMPTY";
-const SHOW  = "SHOW";
+const EMPTY  = "EMPTY";
+const SHOW   = "SHOW";
+const CREATE = "CREATE";
 
 export default function Appointment(props) {
 
   const { mode, transition, back } = useVisualMode(
     props.interview ? SHOW : EMPTY
   );
-  
-  
 
   return(
     <article className="appointment">
       <Header time={props.time} />
-      {mode === EMPTY && <Empty onAdd={() => console.log("Clicked onAdd")} />}
+      {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
       {mode === SHOW && (
         <Show
           student={props.interview.student}
           interviewer={props.interview.interviewer}
         />
-      )}
+        )}
+      {mode === CREATE && <Form interviewers={[]} onCancel={back} />}
     </article>
   );
 }
