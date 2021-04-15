ecefbafcfd3af78a0aeed92d5a8e99ff74ca7cd6
Ted <ted@ted-ubuntu-desktop>
2021-04-14 19:32:34 -0600

    add Promise.all to api calls

--- src/components/Application.js
+++ src/components/Application.js
@@ -6,55 +6,11 @@ import DayList from "components/DayList";
 import Appointment from "components/Appointment";
 
 // stores the api URL for less mess.
-const daysURL = 'http://localhost:8001/api/days';
+const daysURL         = axios.get('http://localhost:8001/api/days');
+const appointmentsURL = axios.get('http://localhost:8001/api/appointments');
+const interviewersURL = axios.get('http://localhost:8001/api/interviewers');
 
-// const appointments = [
-//   {
-//     id: 1,
-//     time: "12pm",
-//   },
-//   {
-//     id: 2,
-//     time: "1pm",
-//     interview: {
-//       student: "Lydia Miller-Jones",
-//       interviewer: {
-//         id: 1,
-//         name: "Sylvia Palmer",
-//         avatar: "https://i.imgur.com/LpaY82x.png",
-//       }
-//     }
-//   },
-//   {
-//     id: 3,
-//     time: "2pm",
-//     interview: {
-//       student: "Archie Cohen",
-//       interviewer: {
-//         id: 2,
-//         name: "Tori Malcolm",
-//         avatar: "https://i.imgur.com/Nmx0Qxo.png",
-//       }
-//     }
-//   },
-//   {
-//     id: 4,
-//     time: "3pm",
-//     interview: {
-//       student: "Maria Boucher",
-//       interviewer: {
-//         id: 3,
-//         name: "Mildred Nazir",
-//         avatar: "https://i.imgur.com/T2WwVfS.png",
-//       }
-//     }
-//   },
-//   {
-//     id: 5,
-//     time: "4pm"
-//   }
-// ];
-
+// component
 export default function Application(props) {
   
   const [state, setState] = useState({
@@ -70,10 +26,11 @@ export default function Application(props) {
 
   // uses a axios to make a get request to the api url
   useEffect(() => {
-    axios.get(daysURL)
-      .then((response) => {
-        setState(prevState => ({...prevState, days: response.data}))
-      })
+   Promise.all([daysURL, appointmentsURL, interviewersURL])
+    .then((all) => {
+      setState(prevState => ({...prevState, days: all[0].data, appointments: all[1].data }));
+    });
+
   }, [])
 
   // uses array.map to map through the appointments list
