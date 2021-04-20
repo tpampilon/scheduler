2481ce4c686274e0c2c65dd390e3087b1be85cf3
Ted <ted@ted-ubuntu-desktop>
2021-04-20 17:25:10 -0600

    add testing complexity in Application.test.js

--- src/__mocks__/axios.js
+++ src/__mocks__/axios.js
@@ -54,8 +54,9 @@ const fixtures = {
 };
 
 export default {
+  
   get: jest.fn(url => {
-    if (url === "/api/days") {
+    if (url === "http://localhost:8001/api/days") {
       return Promise.resolve({
         status: 200,
         statusText: "OK",
@@ -63,7 +64,7 @@ export default {
       });
     }
 
-    if (url === "/api/appointments") {
+    if (url === "http://localhost:8001/api/appointments") {
       /* Resolve appointments data */
       return Promise.resolve({
         status: 200,
@@ -72,7 +73,7 @@ export default {
       });
     }
 
-    if (url === "/api/interviewers") {
+    if (url === "http://localhost:8001/api/interviewers") {
       /* Resolve interviewers data */
       return Promise.resolve({
         status: 200,
@@ -80,5 +81,12 @@ export default {
         data: fixtures.interviewers
       });
     }
+  }),
+
+  put: jest.fn((url) => {
+    return Promise.resolve({
+      status: 204,
+      statusText: "No Content"
+    });
   })
-}a
+};
